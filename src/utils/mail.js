aaaa91f0348d3779c95463de7227ca63f12a21a2
d-sofiika <soniadrozd2709@gmail.com>
2024-10-01 00:18:46 +0200

    updated

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -1,16 +1,20 @@
-import nodemailer from "nodemailer"
-import { SMTP } from "../constants/index.js";
+import nodemailer from 'nodemailer';
+import { SMTP } from '../constants/index.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
+console.log(process.env.SMTP_SERVER);
 const transport = nodemailer.createTransport({
-    host: SMTP.SERVER,
-    port: SMTP.PORT,
-    secure: false,
-     auth: {
+  host: SMTP.SERVER,
+  port: SMTP.PORT,
+  secure: false,
+  auth: {
     user: SMTP.USER,
     pass: SMTP.PASSWORD,
   },
-})
+  
+});
 
 export const sendMail = (message) => {
-    return transport.sendMail(message)
-}
+  return transport.sendMail(message);
+};
